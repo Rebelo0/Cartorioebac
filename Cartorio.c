1768ecf16daae7168b10ae966b0f36db5a2a50d1
Gustavo Rebelo <132093739+Rebelo0@users.noreply.github.com>
2025-05-20 08:58:04 -0300

    Uploud do projeto
    Projeto com as especificações do curso

---
+++ Cartorio.c
@@ -0,0 +1,156 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <string.h>
+
+int registro(){
+	char arquivo[40];
+	char cpf[40];
+	char nome[40];
+	char sobrenome[40];
+	char cargo[40];
+	
+	printf("Digite o CPF: ");
+	scanf("%s", cpf);
+	
+	strcpy(arquivo, cpf);
+	
+	FILE *file;
+	file = fopen(arquivo, "w");
+	fprintf(file, cpf);
+	fclose(file);
+	
+	file = fopen(arquivo, "a");
+	fprintf(file,",");
+	fclose(file);
+	
+	printf("Digite o nome: ");
+	scanf("%s", nome);
+	
+	file = fopen(arquivo, "a");
+	fprintf(file, nome);
+	fclose(file);
+	
+	file = fopen(arquivo, "a");
+	fprintf(file,",");
+	fclose(file);
+	
+	printf("Digite o sobrenome: ");
+	scanf("%s", sobrenome);
+	
+	file = fopen(arquivo, "a");
+	fprintf(file, sobrenome);
+	fclose(file);
+	
+	file = fopen(arquivo, "a");
+	fprintf(file,",");
+	fclose(file);
+	
+	printf("Digite o cargo da pessoa: ");
+	scanf("%s", cargo);
+	
+	file = fopen(arquivo, "a");
+	fprintf(file, cargo);
+	fclose(file);
+	
+	system("pause");
+	
+}
+
+int consulta(){
+	setlocale(LC_ALL, "Portuguese");
+	
+	char cpf[40];
+	char conteudo[200];
+	
+	printf("Digite o CPF: ");
+	scanf("%s", cpf);
+	
+	FILE *file;
+	file = fopen(cpf, "r");
+	
+	if(file == NULL){
+		printf("Não foi localizado.\n");
+		
+	}
+	
+	while(fgets(conteudo, 200, file) != NULL){
+		printf("\nEssas são as informações do usuário: ");
+		printf("%s", conteudo);
+		printf("\n\n");
+	}
+	
+	fclose(file);
+	
+	system("pause");
+		
+}
+
+int deletar(){
+	char cpf[40];
+	
+	printf("Digite o cpf do usuário que você deseja deletar: ");
+	scanf("%s", cpf);
+	
+	remove(cpf);
+	
+	FILE *file;
+	file = fopen(cpf, "r");
+	
+	fclose(file);
+	
+	if(file == NULL){
+		printf("Não existe este usuário \n");
+		system("pause");
+	}
+	
+	
+}
+
+
+
+int main()
+{
+	int opcao = 0;
+	int laco = 1;
+	
+	for (laco=1; laco=1;)
+	{
+	
+		setlocale(LC_ALL, "Portuguese");
+		printf("###### Cartório da EBAC ###### \n\n");
+		printf("Escolha a opcção desejada do menu \n\n");
+		printf("\t1 - Resgistrar nomes\n");
+		printf("\t2 - Consultar nomes\n");
+		printf("\t3 - Deletar nomes\n\n");
+	
+	
+		scanf("%d", &opcao);
+
+		system("cls");
+		
+		switch(opcao)
+		{
+			case 1:
+				registro();
+				break;
+				
+			case 2:
+				consulta();
+				break;
+				
+			case 3:
+				deletar();
+				break;
+				
+			default:
+				printf("Está opção não está disponivel\n");
+				system("pause");
+				break;
+		}
+	
+	}
+	
+}
+
+
